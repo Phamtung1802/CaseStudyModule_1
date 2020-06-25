b15a1757886e50e337f8c128e584ace25ae75a40
unknown <phamtung.0218@gmail.com>
2020-06-25 17:34:35 +0700

    hoan thien nhan vat va hinh nen

---
+++ Object.js
@@ -0,0 +1,18 @@
+﻿ //make a place to hold all image
+ let ImageObjStorage=function(){
+    this.background = new Image();
+    this.bird=new Image();
+    this.bullet= new Image();
+    this.background.src = "PixelArt.png";
+    this.bird.src = "bird.png";
+    this.bullet.src = "bullet.png";
+ }
+ function Drawable() {
+    this.init = function(x, y, width, height) {
+		// Defualt variables
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+}
